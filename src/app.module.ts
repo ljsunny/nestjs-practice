2089bfd2c75f822be1f812a5a08398aaa54a8e19
ljsunny <js961223@naver.com>
2026-02-21 17:02:18 -0800

    feat: install & add structure for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
+    AdminModule,
   ],
 })
 export class AppModule {}
